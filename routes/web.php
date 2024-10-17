19d4b1595550f7a072888802c3ac6b6357d9edd2
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2024-10-17 11:35:50 -0400

    sistemas de rutas configuradas
    sistemas de rutas configurada y adaptada para el prefijo dashboard y el
    controlador DashboardController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('/dashboard')->controller(DashboardController::class)->group(function(){
+    Route::get('staff','staff')->name('dashboard.staff');
+});
