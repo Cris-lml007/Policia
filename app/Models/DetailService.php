ca54b25b68c893270fe0d34ba0645f1ea53bae17
Celeste <celestavaru@gmail.com>
2024-11-10 15:50:43 -0400

    modelos detailservice y groupservice

--- app/Models/DetailService.php
+++ app/Models/DetailService.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class DetailService extends Model
 {
     use HasFactory;
+    public $fillable = [
+        'title',
+        'date',
+        'status',
+        'lat',
+        'long'
+    ];
+
+    public function service(){
+        return $this->hasOne(Service::class);
+    }
+
+    public function groupService(){
+        return $this->hasOne(GroupService::class);
+    }
+    public function users(){
+        return $this->hasMany(User::class);
+    }
 }
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+    public $fillable = [
+        'title',
+        'date',
+        'status',
+        'lat',
+        'long'
+    ];
+
+    public function groupService(){
+        return $this->hasMany(GroupService::class);
+    }
+
+    public function detailService(){
+        return $this->hasMany(DetailService::class);
+    }
 }
