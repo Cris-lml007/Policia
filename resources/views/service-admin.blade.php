af78be7ef64c35c8b8141cd1723ee2f983b9b9ee
Celeste <celestavaru@gmail.com>
2024-11-13 02:29:16 -0400

    Despliegue de datos en tabla y modificaciones de estilo

--- resources/views/service-admin.blade.php
+++ resources/views/service-admin.blade.php
@@ -7,7 +7,14 @@
     <div class="general fondo">
         <div class="container label">
             <h2 class="form_titulo subtitulo">Servicios policiales</h2>
-            <table>
+            <div class="end">
+                <button class="button inline_button">
+                    Sincronizar servicios
+                    <span class="fas fa-sync"></span>
+                </button>
+            </div>
+            
+            <table id="parent">
                 <thead class="table_header">
                     <th>Servicio</th>
                     <th>Estado</th>
@@ -19,16 +26,58 @@
                     <td>Programado</td>
                     <td>-17.946398 / -67.111552</td>
                     <td>
-                        <button class="button inline_button" data-toggle="collapse" href="#details1" aria-expanded="true" aria-controls="details1">
+                        <button class="button inline_button" data-toggle="collapse" data-parent="#parent" href="#details1">
                             Detalles
                             <span class="fas fa-eye button_span"></span>
                         </button>
                     </td>
                 </tr>
-                <tr class="card-body">
+                <tr>
                     <td colspan="4" >
-                        <div class="card card-body collapse"id="details1">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia repellendus ut excepturi molestiae, ducimus sunt earum! Dolorem reprehenderit quia necessitatibus, tempora assumenda eum accusamus eveniet sequi odio eligendi. Consequuntur, quidem ducimus tempore doloremque animi assumenda, ex amet earum fuga tempora doloribus ipsa! Nisi distinctio commodi iste sed voluptate a sequi?
+                        <div class="cont panel-collapse collapse in" data-parent="#parent" id="details1">
+                            <p class="label subtitulo">Personal asignado a la operación:<br></p>
+                            <table>
+                                <thead class="table_header color_header">
+                                    <th>Rango</th>
+                                    <th>Apellido Paterno</th>
+                                    <th>Apellido Materno</th>
+                                    <th>Nombres</th>
+                                    <th>Celular</th>
+                                    <th>Cargo</th>
+                                </thead>
+                                <tr>
+                                    <td>Sargento 1°</td>
+                                    <td>Magne</td>
+                                    <td>Choque</td>
+                                    <td>Aldo</td>
+                                    <td>73847278</td>
+                                    <td>Supervisor</td>
+                                </tr>
+                                <tr>
+                                    <td>Sargento</td>
+                                    <td>Lopez</td>
+                                    <td>Lopez</td>
+                                    <td>José Ignacio</td>
+                                    <td>71853397</td>
+                                    <td>Efectivo</td>
+                                </tr>
+                                <tr>
+                                    <td>Sargento</td>
+                                    <td>Velasquez</td>
+                                    <td>Condori</td>
+                                    <td>Victor Manuel</td>
+                                    <td>73973335</td>
+                                    <td>Efectivo</td>
+                                </tr>
+                                <tr>
+                                    <td>Sargento</td>
+                                    <td>Quispe</td>
+                                    <td>Llanque</td>
+                                    <td>Angel</td>
+                                    <td>68335663</td>
+                                    <td>Efectivo</td>
+                                </tr>
+                            </table>
                         </div>
                     </td>
                 </tr>
@@ -37,16 +86,58 @@
                     <td>Programado</td>
                     <td>-17.968767 / -67.103802</td>
                     <td>
-                        <button class="button inline_button" data-toggle="collapse" href="#details2" aria-expanded="true" aria-controls="details2">
+                        <button class="button inline_button" data-toggle="collapse" data-parent="#parent" href="#details2">
                             Detalles
                             <span class="fas fa-eye button_span"></span>
                         </button>
                     </td>
                 </tr>
-                <tr class="card-body">
+                <tr>
                     <td colspan="4" >
-                        <div class="card card-body collapse"id="details2">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia repellendus ut excepturi molestiae, ducimus sunt earum! Dolorem reprehenderit quia necessitatibus, tempora assumenda eum accusamus eveniet sequi odio eligendi. Consequuntur, quidem ducimus tempore doloremque animi assumenda, ex amet earum fuga tempora doloribus ipsa! Nisi distinctio commodi iste sed voluptate a sequi?
+                        <div class="cont panel-collapse collapse in" data-parent="#parent" id="details2">
+                            <p class="label subtitulo">Personal asignado a la operación:<br></p>
+                            <table>
+                                <thead class="table_header color_header">
+                                    <th>Rango</th>
+                                    <th>Apellido Paterno</th>
+                                    <th>Apellido Materno</th>
+                                    <th>Nombres</th>
+                                    <th>Celular</th>
+                                    <th>Cargo</th>
+                                </thead>
+                                <tr>
+                                    <td>Sargento 1°</td>
+                                    <td>Magne</td>
+                                    <td>Choque</td>
+                                    <td>Aldo</td>
+                                    <td>73847278</td>
+                                    <td>Supervisor</td>
+                                </tr>
+                                <tr>
+                                    <td>Sargento</td>
+                                    <td>Lopez</td>
+                                    <td>Lopez</td>
+                                    <td>José Ignacio</td>
+                                    <td>71853397</td>
+                                    <td>Efectivo</td>
+                                </tr>
+                                <tr>
+                                    <td>Sargento</td>
+                                    <td>Velasquez</td>
+                                    <td>Condori</td>
+                                    <td>Victor Manuel</td>
+                                    <td>73973335</td>
+                                    <td>Efectivo</td>
+                                </tr>
+                                <tr>
+                                    <td>Sargento</td>
+                                    <td>Quispe</td>
+                                    <td>Llanque</td>
+                                    <td>Angel</td>
+                                    <td>68335663</td>
+                                    <td>Efectivo</td>
+                                </tr>
+                            </table>
                         </div>
                     </td>
                 </tr>
