7e366d1ad356bfd5b107ef92c8bb21f98dbca24f
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2024-11-27 13:52:35 -0400

    adicion de etiqueta a layouts
    se añadio una etiqueda especial a falta de un SSL

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
