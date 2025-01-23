1785c7f7b11b39cd828f58f3949bd4c8e5802152
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2025-01-22 23:32:58 -0400

    injeccion livewire corregido

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,18 @@
 use App\Http\Controllers\DashboardController;
 use App\Livewire\ActivateAccount;
 use App\Livewire\Counter;
+use App\Livewire\Profile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
 Route::get('/counter', Counter::class);
 
 Livewire::setScriptRoute(function($handle){
-    return Route::get(env('APP_URL') . '/livewire/livewire.js',$handle);
+    return Route::get(env('IP_PUBLIC',env('APP_URL')) . '/livewire/livewire.js',$handle);
 });
 
 Livewire::setUpdateRoute(function($handle){
-    return Route::post(env('APP_URL').'/livewire/update',$handle);
+    return Route::post(env('IP_PUBLIC',env('APP_URL')).'/livewire/update',$handle);
 });
 
 Route::get('/', function () {
@@ -35,5 +36,6 @@ Route::prefix('/dashboard')->middleware('auth')->controller(DashboardController:
     Route::get('/service/{service}','getService')->name('dashboard.getService');
     Route::get('/reports/get','getReport')->name('dashboard.report');
     Route::get('/token');
+    Route::get('/profile',Profile::class)->name('dashboard.profile');
 });
 
