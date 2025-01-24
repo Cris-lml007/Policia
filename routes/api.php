c14f9609e7558cb6830a3b0ee2c4c1be1eb94293
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2025-01-23 23:16:10 -0400

    correccion de api.php

--- routes/api.php
+++ routes/api.php
@@ -22,48 +22,47 @@ Route::middleware('auth:sanctum')->group(function(){
         return response("cannot delete service",202);
     })->can('service');
 
-});
-Route::post('/service',function(Request $request){
-    $r = $request->all();
-    // return response()->json($r);
-    try {
-        $service = Service::create([
-            'cod' => $r['codigo'],
-            'title' => $r['servicio'],
-            'date_start' => $r['fecha_inicio'],
-            'date_end' => $r['fecha_fin'],
-            'lat' => $r['latitud'],
-            'long' => $r['longitud']
-        ]);
-    } catch (\Throwable $th) {
-        return response("there was an error creating service",400);
-    }
-    $data1 = [];
-    foreach ($r['grupos'] as $value) {
-        if(!empty($value['encargado'])){
-            if(!User::createForAPI($value['encargado'])){
-                $service->destroy();
-                return response("there was an error creating service",400);
-            }
+    Route::post('/service',function(Request $request){
+        $r = $request->all();
+        try {
+            $service = Service::create([
+                'cod' => $r['codigo'],
+                'title' => $r['servicio'],
+                'date_start' => $r['fecha_inicio'],
+                'date_end' => $r['fecha_fin'],
+                'lat' => $r['latitud'],
+                'long' => $r['longitud']
+            ]);
+        } catch (\Throwable $th) {
+            return response("there was an error creating service",400);
         }
-        $group = GroupService::create([
-            'service_id' => $service->id,
-            'user_ci' => empty($value['encargado']) ? null : $value['encargado']
-        ]);
-        foreach ($value['integrantes'] as $v) {
-            if(!User::createForAPI($v)){
-                $service->destroy();
-                return response("there was an error creating service",400);
+        $data1 = [];
+        foreach ($r['grupos'] as $value) {
+            if(!empty($value['encargado'])){
+                if(!User::createForAPI($value['encargado'])){
+                    $service->destroy();
+                    return response("there was an error creating service",400);
+                }
             }
-            $data1 [] = [
-                'group_service_id' => $group->id,
-                'user_ci' => $v,
-                'service_id' => $service->id
-            ];
+            $group = GroupService::create([
+                'service_id' => $service->id,
+                'user_ci' => empty($value['encargado']) ? null : $value['encargado']
+            ]);
+            foreach ($value['integrantes'] as $v) {
+                if(!User::createForAPI($v)){
+                    $service->destroy();
+                    return response("there was an error creating service",400);
+                }
+                $data1 [] = [
+                    'group_service_id' => $group->id,
+                    'user_ci' => $v,
+                    'service_id' => $service->id
+                ];
+            }
+            DetailService::upsert($data1,[],[]);
         }
-        DetailService::upsert($data1,[],[]);
-    }
-    return response()->json(['message' => 'service created successfully'],201);
+        return response()->json(['message' => 'service created successfully'],201);
+    })->can('service');
 });
 
 Route::get('/checkhealth',function(){
